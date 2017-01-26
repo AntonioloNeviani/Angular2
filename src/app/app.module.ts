7a3bd74ee17905aabb7c9eec6ef09ed85e71bb60
AntonioloNeviani <jorgeant2000@hotmail.com>
2017-01-26 15:39:49 -0600

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContentComponent
+    ContentComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
